4a6d34c7f254eb819a98b08d5a7b0ff1f54fa154
marko.popyk <marko.popyk@raisin.com>
2026-02-22 23:27:43 +0200

    Rename main to master

--- src/main/java/org/mongot/simplewebserver/api/SimpleEndpoint.java
+++ src/main/java/org/mongot/simplewebserver/api/SimpleEndpoint.java
@@ -1,16 +1,19 @@
 package org.mongot.simplewebserver.api;
 
 import jakarta.websocket.server.PathParam;
+import lombok.extern.slf4j.Slf4j;
 import org.mongot.simplewebserver.api.dto.SimpleRequest;
 import org.mongot.simplewebserver.api.dto.SimpleResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SimpleEndpoint {
 
     @GetMapping
     public SimpleResponse message(@PathParam("message") SimpleRequest request) {
+        log.info("Message endpoint called");
         return new SimpleResponse(request.message());
     }
 }
